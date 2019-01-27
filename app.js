aad1e4fe43611326e497d21fa46fb6f3a7571f87
marleigh <volcana@gmail.com>
2019-01-26 18:12:35 -0800

    JS Built
    mostly..

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-console.log("It works!");
+// listen for submit
+
+document.getElementById('loan-form').addEventListener('submit', function(e){
+
+    e.preventDefault();
+});
+
+function calculateResults() {
+   //UI variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //compute monthly payments
+    const x = Math.pow(1+ calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    } else {
+        showError('Please check your numbers');
+    }
+
+}
+
+function showError(error) {
+    const errorDiv = document.createElement('div');
+
+    //get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+
+    // add classes
+    errorDiv.className = 'alert alert-danger';
+
+    //create text node and append
+    errorDiv.appendChild(document.createTextNode(error));
+
+    //insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    //set timeout for error
+    //3 seconds
+
+    setTimeout(clearError, 3000);
+
+}
+
+function clearError() {
+    document.querySelector('.alert').remove();
+}
